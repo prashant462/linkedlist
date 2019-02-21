4b0ba4165a546f1cc4024b51c9e4cc80e6ff3403
prashant462 <prashantmalhotra462@gmail.com>
2019-02-21 08:28:38 +0530

    add constructor for node

--- src/singlylinkedlists.java
+++ src/singlylinkedlists.java
@@ -2,5 +2,11 @@ public class singlylinkedlists {
     private static class Node{
         private int data;
         private Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+
     }
 }
